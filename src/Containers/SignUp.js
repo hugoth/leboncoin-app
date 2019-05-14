911dfd137933af38a918ea7161fbb0046d0eb616
Hugo Tessier <htessierhuort@gmail.com>
2019-05-14 17:36:08 +0200

    add Style on SignUP

--- src/Containers/SignUp.js
+++ src/Containers/SignUp.js
@@ -53,53 +53,112 @@ class SignUp extends Component {
       );
     } else {
       return (
-        <div>
-          <div>Authentication</div>
-          <form className="register-form" onSubmit={this.handleSubmit}>
-            <div>
-              <p>Pseudo</p>
-              <label htmlFor="email">Email</label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div>
-              <label>Adresse Mail</label>
-              <input
-                id="email"
-                name="email"
-                type="text"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div>
-              <label>Mot de passe</label>
-              <input
-                id="password"
-                name="password"
-                type="text"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-              <label>Confirmer mot de passe</label>
-              <input
-                id="password2"
-                name="password2"
-                type="text"
-                value={this.state.password2}
-                onChange={this.handleChange}
-              />
-            </div>
-            <button>Submit</button>
-          </form>
+        <div className="container-authentification">
+          <div className="authentification-left-top">
+            <h2>Pourquoi créer un compte ?</h2>
+            <div classNAme="left-logo-text">
+              <div className="text-logo">
+                <div className="paragraphe-1">
+                  <i className="far fa-clock" />
+                  <div className="text-rigt-of-logo">
+                    <h4>Gagnez du temps</h4>
+                    <p>
+                      Publiez vos annonces rapidement, avec vos informations
+                      pré-remplies chaque fois que vous souhaitez déposer une
+                      nouvelle annonce.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  {" "}
+                  <div className="paragraphe-1">
+                    <i className="fas fa-bell" />
+                    <div className="text-rigt-of-logo">
+                      <h4>Soyez les premiers informés</h4>
+                      <p>
+                        créez des alertes Immo ou Emploi et ne manquez pas
+                        jamais l'annonce qui vous intéresse.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="paragraphe-1">
+                  <i className="far fa-eye" />
+                  <div className="text-rigt-of-logo-eye">
+                    <h4>Visibilité</h4>
+                    <p>
+                      Suivez les statistiques de vos annonces(nombre de fois où
+                      votre annonce a été vue, nombre de contacts recus).
+                    </p>
+                  </div>
+                </div>
 
-          <p>{this.state.username}</p>
-          <p>{this.state.email}</p>
+                <div />
+              </div>
+            </div>
+          </div>
+          <div className="authentification-right">
+            <h2>Créez un compte</h2>
+            <form className="register-form" onSubmit={this.handleSubmit}>
+              <div>
+                <h4>Pseudo</h4>
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div>
+                <h4>Adresse Mail</h4>
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div>
+                <div className="password">
+                  <div>
+                    <h4>Mot de passe</h4>
+                    <input
+                      id="password"
+                      name="password"
+                      type="text"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div>
+                    <h4>Confirmer mot de passe</h4>
+
+                    <input
+                      id="password2"
+                      name="password2"
+                      type="text"
+                      value={this.state.password2}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="checkbox">
+                  <input type="checkbox" />
+                  <p>
+                    Je souhaite recevoir des offres des partenaires du site
+                    leboncoin succeptibles de m'intéresser
+                  </p>
+                </div>
+                <div className="checkbox">
+                  <input type="checkbox" />
+                  <p>J'accepte les Conditions Générales de Vente </p>
+                </div>
+                <button>Créer mon Compte Personnel</button>
+              </div>
+            </form>
+          </div>
         </div>
       );
     }
@@ -107,3 +166,59 @@ class SignUp extends Component {
 }
 
 export default SignUp;
+
+{
+  /* <div>
+        <div className="container-authentification">
+         
+                
+          <div className="authentification-right">
+            <h2>Créez un compte</h2>
+            <form className="register-form" onSubmit={this.handleSubmit}>
+              <div>
+                <h4>Pseudo</h4>
+                <input
+                  type="text"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div>
+                <h4>Adresse Mail</h4>
+                <input />
+              </div>
+              <div>
+                <div className="password">
+                  <div>
+                    <h4>Mot de passe</h4>
+                    <input />
+                  </div>
+                  <div>
+                    <h4>Confirmer le mot de passe</h4>
+                    <input />
+                  </div>
+                </div>
+              </div>
+
+              <div className="checkbox">
+                <input type="checkbox" />
+                <p>
+                  Je souhaite recevoir des offres des partenaires du site
+                  leboncoin succeptibles de m'intéresser
+                </p>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" />
+                <p>"J'accepte les"</p>
+                <p>Conditions Générales de Vente</p>
+              </div>
+              <button>Créer mon Compte Personnel</button>
+            </form>
+          </div>
+
+          <p>{this.state.username}</p>
+        </div>
+      </div>
+    );
+  } */
+}
